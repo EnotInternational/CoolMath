2adeae6edbb0bd9986a473bb1eb0827d54c518a4
KOKODOBA223 <vshabatura27@rambler.ru>
2025-02-16 22:01:17 +0300

    UI (tools, play-stop )

--- Assets/UI/PlayVideo.cs
+++ Assets/UI/PlayVideo.cs
@@ -8,16 +8,16 @@ public class PlayVideo1 : MonoBehaviour
 {
     public RawImage rawImage;
     public VideoPlayer videoPlayer;
-    void Start(){
+    void OnEnable(){
         StartCoroutine(PlayVideos());
     }
     IEnumerator PlayVideos(){
         videoPlayer.Prepare();
-        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        WaitForSeconds waitForSeconds = new WaitForSeconds(0);
         while (!videoPlayer.isPrepared)
         {
             yield return waitForSeconds;
-            break;
+            
         }
         rawImage.texture = videoPlayer.texture;
         videoPlayer.Play();
