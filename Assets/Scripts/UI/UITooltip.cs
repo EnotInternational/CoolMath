59c189e6214c94106c2e13c05ebfb668a74390c1
EnotInternational <155220593+EnotInternational@users.noreply.github.com>
2025-02-21 22:15:22 +0300

    Binds fix, tooltip fix

--- Assets/Scripts/UI/UITooltip.cs
+++ Assets/Scripts/UI/UITooltip.cs
@@ -70,6 +70,8 @@ public class UITooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     }
     private void Hide()
     {
+        if(!tooltipTransform)
+            return;
         tooltipTransform.gameObject.SetActive(false);
     }
 
